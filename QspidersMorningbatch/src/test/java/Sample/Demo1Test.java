b7870b23150759dd186823baac5f2454eafe7673
rathinreddy1402 <rathinreddy1402@gmail.com>
2023-02-02 11:16:02 +0530

    1

---
+++ QspidersMorningbatch/src/test/java/Sample/Demo1Test.java
@@ -0,0 +1,11 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+@Test
+public void t1()
+{
+	System.out.println("hi");
+}
+}
---
+++ QspidersMorningbatch/src/test/java/Sample/Demo2Test.java
@@ -0,0 +1,11 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class Demo2Test {
+@Test
+public void t2()
+{
+	System.out.println("bye");
+}
+}
